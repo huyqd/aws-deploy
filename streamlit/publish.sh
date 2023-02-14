3ea2ad8cda7ea4176b6d145a5d6315951ace99b0
huyqd <quanghuy.dang210@gmail.com>
2023-02-14 22:14:32 +0100

    login to ecr

--- streamlit/publish.sh
+++ streamlit/publish.sh
@@ -1,3 +1,4 @@
 docker build -t streamlit .
 docker tag streamlit:latest $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/aws-deploy:streamlit
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
 docker push $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/aws-deploy:streamlit
